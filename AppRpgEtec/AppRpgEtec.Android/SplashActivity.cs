a41e913800db17c2ef19054a4e269e0da228cd2c
IgorRibe <rc.igorr@gmail.com>
2022-08-16 22:24:27 -0300

    telaetec

---
+++ AppRpgEtec/AppRpgEtec.Android/SplashActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Util;
+
+
+namespace AppRpgEtec.Droid
+{
+    [Activity(Label = "App RPG", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
